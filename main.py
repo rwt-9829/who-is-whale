3ec1fa0e1dfe3a27bddfc6f331ea3a3063432ea1
rwt-9829 <hansenhe2008@gmail.com>
2025-04-30 19:32:08 -0700

    remove meta tag

--- main.py
+++ main.py
@@ -4,9 +4,6 @@ from app.stats_display import show_player_stats
 from parser.log_parser import parse_log_file, extract_stats
 
 st.set_page_config(page_title="Poker Hand History Analyzer")
-st.html(
-    '<meta name="google-site-verification" content="ofFL2OsTyaWtskAzYjg1Z5nvqIjSjT6kkyj9zw35CqA" />'
-)
 #st.title("PokerNow.Club Log Analysis")
 st.header("Poker Hand History Analyzer")
 st.text("Analyze online poker logs to extract detailed player behavior statistics, including c-bets, donk bets, probes, and fold frequencies at each street. Ideal for improving strategy and gaining insights from historical hands.")
